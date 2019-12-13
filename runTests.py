b6a26abb1d46385c510855958fc0e656021ee80b
django-query-profiler <django.query.profiler@gmail.com>
2019-12-12 21:26:08 -0800

    Enabling setup.py test to invoke runTests.py

--- runTests.py
+++ runTests.py
@@ -6,10 +6,15 @@ import django
 from django.conf import settings
 from django.test.utils import get_runner
 
-if __name__ == "__main__":
+
+def run_tests():
     os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.integration.setup.test_settings'
     django.setup()
     TestRunner = get_runner(settings)
     test_runner = TestRunner()
     failures = test_runner.run_tests(["tests"])
     sys.exit(bool(failures))
+
+
+if __name__ == "__main__":
+    run_tests()

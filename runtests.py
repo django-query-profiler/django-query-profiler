6158bd943d8d1553cf7eed8592010b28a8bbd585
django-query-profiler <django.query.profiler@gmail.com>
2019-12-17 21:28:15 -0800

    a tox configuration that works by running one django version on all three databases

--- runtests.py
+++ runtests.py
@@ -8,14 +8,39 @@ from django.conf import settings
 from django.test.runner import DiscoverRunner
 from django.test.utils import get_runner
 
+ARGS_TO_SETTINGS_FILE = {
+    'sqlite': 'tests.testapp.sqlite_settings',
+    'mysql': 'tests.testapp.mysql_settings',
+    'postgres': 'tests.testapp.postgres_settings'
+}
+
+
+def _unit_tests(test_runner_instance: DiscoverRunner) -> int:
+    failure_count = test_runner_instance.run_tests(['tests/unit'])
+    return failure_count
+
+
+def _integration_test(test_runner_instance: DiscoverRunner) -> int:
+    failure_count = test_runner_instance.run_tests(['tests/integration'])
+    return failure_count
+
 
 def run_tests():
-    os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.testapp.test_settings'
+    db_name: str = sys.argv[1] if len(sys.argv) > 1 and sys.argv[1] != 'test' else 'sqlite'
+    settings_file: str = ARGS_TO_SETTINGS_FILE[db_name]
+    print('==' * 80)
+    print(db_name)
+    print(settings_file)
+    print('==' * 80)
+
+    os.environ['DJANGO_SETTINGS_MODULE'] = settings_file
     django.setup()
     test_runner_class: Type[DiscoverRunner] = get_runner(settings)
     test_runner_instance: DiscoverRunner = test_runner_class(verbosity=2)
-    failures = test_runner_instance.run_tests(["tests"])
-    sys.exit(bool(failures))
+
+    integration_test_failure_count = _integration_test(test_runner_instance)
+    unit_test_failure_count = _unit_tests(test_runner_instance)
+    sys.exit(integration_test_failure_count + unit_test_failure_count)
 
 
 if __name__ == "__main__":
---
+++ tests/testapp/oracle_settings.py
@@ -0,0 +1,20 @@
+import os
+
+from django_query_profiler.settings import *
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+INSTALLED_APPS = (
+    'django_query_profiler',
+    'tests.testapp',
+)
+
+SECRET_KEY = 'dummy'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django_query_profiler.django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}

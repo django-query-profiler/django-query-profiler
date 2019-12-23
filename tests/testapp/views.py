1c7e197b058a7938b62ec2921ea94dbb5b6adba3
django-query-profiler <django.query.profiler@gmail.com>
2019-12-23 12:33:41 -0800

    trying codecov #2

--- tests/testapp/views.py
+++ tests/testapp/views.py
@@ -16,7 +16,6 @@ def index(request: WSGIRequest) -> HttpResponse:
     [pizza.spicy_toppings_db_filtering() for pizza in Pizza.objects.prefetch_related('toppings').all()]
     [pizza.spicy_toppings_python_filtering() for pizza in Pizza.objects.prefetch_related('toppings').all()]
     [pizza.toppings_of_best_pizza_serving_restaurants() for pizza in Pizza.objects.prefetch_related('toppings').all()]
-
     list(Restaurant.objects.all())
 
     return HttpResponse("Made all the db calls, now test the calls made via profiler")
